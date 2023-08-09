70a3c173d3f33367ca396999d5d453137dd1412b
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-08-09 17:41:30 +0800

    feat: create some files

---
+++ src/packages/Distance.ts
@@ -0,0 +1,12 @@
+/**
+ * @module distance
+ * @description
+ * - Returns the distance between two points.(Supports both 2D and 3D points on the Sphere and the Cartesian plane)
+ * - 距离模块，返回两点之间的距离（支持球面和笛卡尔平面上的2D和3D点）
+ */
+
+// Path: src\packages\Distance.ts
+
+// 球面坐标系（经纬度）上两点之间的距离，球面形状为正球
+
+// 定义距离计算接口
---
+++ src/packages/Referencing.ts
@@ -0,0 +1,6 @@
+/**
+ * @module Referencing
+ * @description
+ * - Provides a set of functions for reference coordinate system conversion.
+ * - 提供一组用于（管理）参考坐标系转换的函数
+ */
