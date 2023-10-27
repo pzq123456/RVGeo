7a8fe979db276ce1cd00f92d901c5b25b63f9e7a
panzhiqing <1812673119@qq.com>
2023-10-27 21:24:54 +0800

    fix

--- docs/tutorials/doc/meta.js
+++ docs/tutorials/doc/meta.js
@@ -1,77 +1,77 @@
 export const metalist = [
     {
         "title" : "Example1",
-        "path" : "/RVGeo/doc/example1.md",
+        "path" : "doc/example1.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
     },
     {
         "title" : "Example2",
-        "path" : "/RVGeo/doc/example2.md",
+        "path" : "doc/example2.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
     },
     {
         "title" : "Example3",
-        "path" : "/RVGeo/doc/example3.md",
+        "path" : "doc/example3.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
     },
     {
         "title" : "Example4",
-        "path" : "/RVGeo/doc/example4.md",
+        "path" : "doc/example4.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
     },
     {
         "title" : "Example5",
-        "path" : "/RVGeo/doc/example5.md",
+        "path" : "doc/example5.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
     },
     {
         "title" : "Example6",
-        "path" : "/RVGeo/doc/example6.md",
+        "path" : "doc/example6.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
     },
     {
         "title" : "Example7",
-        "path" : "/RVGeo/doc/example7.md",
+        "path" : "doc/example7.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
     },
     {
         "title" : "Example8",
-        "path" : "/RVGeo/doc/example8.md",
+        "path" : "doc/example8.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
     },
     {
         "title" : "Example9",
-        "path" : "/RVGeo/doc/example9.md",
+        "path" : "doc/example9.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
     },
     {
         "title" : "Example10",
-        "path" : "/RVGeo/doc/example10.md",
+        "path" : "doc/example10.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
     },
     {
         "title" : "Example11",
-        "path" : "/RVGeo/doc/example11.md",
+        "path" : "doc/example11.md",
         "date" : "2023-10-27",
         "tag" : "info",
         "command": "cd"
