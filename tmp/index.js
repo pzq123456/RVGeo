36072f2c58264509b7f6bb5b952f302b0a74cf99
潘志清 <82391775+pzq123456@users.noreply.github.com>
2024-04-07 23:38:55 +0800

    Update algorithm section with shortest path algorithm and references

---
+++ tmp/index.js
@@ -0,0 +1,20 @@
+import * as Vector from './src/base.js'
+import {grid as Raster} from './src/grid.js'
+import {Stastic} from './src/grid.js'
+import * as Renderer from './src/display.js'
+import * as Creator from './src/canvas.js'
+import * as Learn from './src/learn.js'
+import * as Test from './src/test.js'
+import * as pan from './src/pan.js'
+
+export {
+    Vector,
+    Raster,
+    Stastic,
+    Renderer,
+    Creator,
+    Learn,
+    Test,
+    pan
+}
+
